8d321b82b1b26c0f4811272c1da11c030be787a8
Aashishrastogi <113436542+Aashishrastogi@users.noreply.github.com>
2023-01-23 23:23:50 +0530

    SpringBootFilesLoaded (#5)
    * latest commit by aashish
    
    * SpringFilesLoaded

--- README.md
+++
@@ -1,2 +0,0 @@
-# Final_Project_CDAC-SEPT22
-Course Final-project PGDAC SEPT 22
---
+++ src/main/java/com/example/demo/Springtrail2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springtrail2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springtrail2Application.class, args);
+	}
+
+}
