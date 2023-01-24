4166bcde3de82ea775c99de779aee7e09c43ac36
Aashishrastogi <113436542+Aashishrastogi@users.noreply.github.com>
2023-01-24 22:50:07 +0530

    17 01 2023 (#8)
    * latest commit by aashish
    
    * SpringFilesLoaded
    
    * changes made by aashish from eclipse
    
    * new changes

--- src/main/java/com/example/demo/Springtrail2Application.java
+++ src/main/java/com/example/demo/Springtrail2Application.java
@@ -9,6 +9,9 @@ public class Springtrail2Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Springtrail2Application.class, args);
 
+
+		System.out.print("these are the new changes");
+
 		
 		System.out.print("theses are changes made by Aashish by importsthe entire project in the eclipse from intelij and from github");
 	}
